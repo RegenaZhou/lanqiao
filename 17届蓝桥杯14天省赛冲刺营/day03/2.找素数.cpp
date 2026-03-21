843091ce84024a11ec2386a18cc565ac0502035b
RegenaZhou <RegenaZhou2005@outlook.com>
2026-03-21 23:31:54 +0800

    Add files via upload

---
+++ 17届蓝桥杯14天省赛冲刺营/day03/2.找素数.cpp
@@ -0,0 +1,51 @@
+﻿/*
+题目描述
+给定一个区间 [a,b]，请你求出该区间内有多少个素数。
+
+输入描述
+输入共一行，包含两个整数 a,b。
+
+2≤a≤b≤2147483647，b−a≤1000000
+
+输出描述
+输出一个整数，表示答案。
+
+输入输出样例
+示例
+输入
+2 6
+
+输出
+3*/
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    ll a, b, ans = 0, n;
+    cin >> a >> b;
+    n = b - a + 1;
+    vector<bool> arr(n, true);
+    for (ll i = 2; i * i <= b; i++)
+    {
+        ll j = max(i * 2, (a + i - 1) / i * i);
+        for (; j <= b; j += i)
+        {
+            arr[j - a] = false;
+        }
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        if (arr[i])
+        {
+            ans++;
+        }
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
