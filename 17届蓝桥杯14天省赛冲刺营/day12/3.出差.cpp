7edd5a7bc358b731df1658df2bc05c1051df7fe8
RegenaZhou <RegenaZhou2005@outlook.com>
2026-07-17 23:59:26 +0800

    Implement Dijkstra's algorithm for city travel time

--- 17届蓝桥杯14天省赛冲刺营/day12/3.出差.cpp
+++ 17届蓝桥杯14天省赛冲刺营/day12/3.出差.cpp
@@ -62,6 +62,7 @@ int dijkstra(){
     else return dist[n];
 }
 int main(){
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
     cin>>n>>m;
     for(int i=1;i<=n;i++){
         scanf("%d",&stay[i]);
