7f4e4196f62e9a9eda4929200393cdda41e179a2
RegenaZhou <RegenaZhou2005@outlook.com>
2026-07-21 00:09:07 +0800

    Update 4.染色时间.cpp

--- 17届蓝桥杯14天省赛冲刺营/day12/4.染色时间.cpp
+++ 17届蓝桥杯14天省赛冲刺营/day12/4.染色时间.cpp
@@ -2,10 +2,10 @@
 问题描述
 小蓝有一个 n 行 m 列的白色棋盘, 棋盘的每一个方格都可以被染成彩色。
 
-每个方格有一个染色时间 tij, 不同方格的染色时间可能不同。如果一个方格被触发了染色, 这个方格就会在 tij 秒之后变成彩色, 然后将自己上下左右四个方向相邻的方格触发染色。
-每个方格只能被触发染色一次, 第一次触发之后的触发为无效触发。
+每个方格有一个染色时间 tij, 不同方格的染色时间可能不同。如果一个方 格被触发了染色, 这个方格就会在 tij 秒之后变成彩色, 然后将自己上下左右四 个方向相邻的方格触发染色。
+每个方格只能被触发染色一次, 第一次触发之后 的触发为无效触发。
 
-给定每个方格的染色时间, 在时刻 0 触发第一行第一列的方格染色, 请问多长时间后整个棋盘完成染色。
+给定每个方格的染色时间, 在时刻 0 触发第一行第一列的方格染色, 请问 多长时间后整个棋盘完成染色。
 
 输入格式
 输入的第一行包含两个整数 n,m, 分别表示棋盘的行数和列数。
@@ -33,52 +33,66 @@
 对于 60 的评测用例, 1≤n,m≤50 。
 
 对于所有评测用例, 1≤n,m≤500,1≤tij≤1000 。*/
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
-int n,m;
-int result = 0;
-const int N = 505;
-int mp[N][N];//记录每个节点染色所需时间
-bool st[N][N];//记录是否染过该节点
-int dx[]={-1,0,1,0};
-int dy[]={0,1,0,-1};
+int n, m;
+int ans = 0;
+int arr[510][510];
+bool vis[510][510];
+int dx[] = { -1,0,1,0 };
+int dy[] = { 0,1,0,-1 };
 
-
-struct t{
-  int x,y,tt;
+struct Node
+{
+    int x, y, ti;
 };
 
-struct cmp{
-    bool operator()(const t& a, const t& b) const {
-        return a.tt > b.tt;
+struct cmp
+{
+    bool operator()(const Node& a, const Node& b) const
+    {
+        return a.ti > b.ti;
     }
 };
 
-
-void bfs(){
-  priority_queue<t,vector<t>,cmp> q;//结合cmp维护的是所用时间最少的优先队列，用的时间最少的排在前面
-  st[0][0] = true;
-  q.push({0,0,mp[0][0]});
-  while(!q.empty()){
-    t mid = q.top();
-    q.pop();
-    result = max(result,mid.tt);
-    for(int i=0;i<4;i++){
-      int xx = mid.x + dx[i],yy = mid.y + dy[i];
-      if(xx<0||xx>=n||yy<0||yy>=m) continue;
-      if(st[xx][yy]) continue;
-      st[xx][yy] = true;
-      q.push({xx,yy,mid.tt+mp[xx][yy]});
+void bfs()
+{
+    priority_queue<Node, vector<Node>, cmp> pq;
+    vis[0][0] = true;
+    pq.push({ 0,0,arr[0][0] });
+    while (!pq.empty())
+    {
+        Node tmp = pq.top();
+        pq.pop();
+        ans = max(ans, tmp.ti);
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = tmp.x + dx[i], ny = tmp.y + dy[i];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if (vis[nx][ny]) continue;
+            vis[nx][ny] = true;
+            pq.push({ nx,ny,tmp.ti + arr[nx][ny] });
+        }
     }
-  }
 }
 
 int main()
 {
-  cin>>n>>m;
-  for(int i=0;i<n;i++)
-    for(int j=0;j<m;j++)  cin>>mp[i][j];
-  bfs();
-  cout<<result<<'\n';
-  return 0;
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    bfs();
+    cout << ans << '\n';
+
+    return 0;
 }
